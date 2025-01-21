579e51dfe26cc32c2a96f6aa6c1df260f2f88203
muzefa01 <muzefaabbas118@gmail.com>
2025-01-21 12:00:45 +0000

    1v1 battle system with socket

--- src/battle.js
+++ src/battle.js
@@ -1,63 +1,50 @@
-class Spirit {
-    constructor(team1, team2) {
-        // this.name = name
-        // this.speed = speed
-        // this.attack = attack
-        // this.defence = defence
-        // this.currentHealth = maxHealth
-        // this.points = 0
-        // this.ticker = 0
-
-        this.team1 = team1.map(spirit => new Spirit(spirit)); 
-        this.team2 = team2.map(spirit => new Spirit(spirit));
-        this.ticker = 0; 
-        this.currentCombatants = [this.team1[0], this.team2[0]]; 
-        this.winner = null; 
+class Battle {
+    constructor(spirit1, spirit2, roomCode, io) {
+      this.spirit1 = spirit1; 
+      this.spirit2 = spirit2; 
+      this.ticker1 = 0;
+      this.ticker2 = 0;
+      this.roomCode = roomCode;
+      this.io = io;
+      this.interval = null;
     }
-
+  
+    start() {
+      this.interval = setInterval(() => this.incrementTicker(), 100);
+    }
+  
     incrementTicker() {
-        this.currentCombatants.forEach(combatant => combatant.ticker());
+      this.ticker1 += this.spirit1.stats.spd;
+      this.ticker2 += this.spirit2.stats.spd;
+  
+      if (this.ticker1 >= 100) {
+        this.resolveRound(this.spirit1, this.spirit2);
+        this.ticker1 -= 100;
       }
-
-      resolveRound() {
-        const [combatant1, combatant2] = this.currentCombatants;
-    
-        if (combatant1.ticker >= 100 && combatant2.ticker >= 100) {
-          // Higher ticker goes first
-          if (combatant1.ticker > combatant2.ticker) {
-            this.attack(combatant1, combatant2);
-          } else {
-            this.attack(combatant2, combatant1);
-          }
-        } else if (combatant1.ticker >= 100) {
-          this.attack(combatant1, combatant2);
-        } else if (combatant2.ticker >= 100) {
-          this.attack(combatant2, combatant1);
-        }
+      if (this.ticker2 >= 100) {
+        this.resolveRound(this.spirit2, this.spirit1);
+        this.ticker2 -= 100;
       }
-    
-
-
-
-
-    // incrementing ticker by its corresponding speed
-    ticker() {
-        this.ticker += this.speed
     }
-
-    attack(opponent) {
-        const damage = (this.attack - opponent.defence)
-        opponent.currentHealth -= damage
-
-        if (opponent.currentHealth <= 0) { // death condition
-            return true
-        }
-        return false
+  
+    resolveRound(attacker, defender) {
+      const damage = Math.max(attacker.stats.atk - defender.stats.def, 0);
+      defender.stats.hp -= damage;
+  
+      this.io.to(this.roomCode).emit('updateHealth', {
+        attackerName: attacker.name,
+        defenderName: defender.name,
+        damage,
+        defenderHP: defender.stats.hp,
+      });
+  
+      if (defender.stats.hp <= 0) {
+        clearInterval(this.interval);
+        const winner = attacker === this.spirit1 ? 0 : 1; // 0 for player 1, 1 for player 2
+        this.io.to(this.roomCode).emit('battleResult', winner);
+      }
     }
-
-    
-
-    resetTicker() {
-        this.ticker = 0
-    }
-}Z
+  }
+  
+  export { Battle };
+  
