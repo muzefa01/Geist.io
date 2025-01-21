59cc864f510aa5844b926aab33f54ce6dbf2ec51
muzefa01 <muzefaabbas118@gmail.com>
2025-01-21 09:45:27 +0000

    Implement battle system 3v3  spirits

--- src/battle.js
+++ src/battle.js
@@ -1,13 +1,45 @@
 class Spirit {
-    constructor(name, speed, attack, defence, maxHealth) {
-        this.name = name
-        this.speed = speed
-        this.attack = attack
-        this.defence = defence
-        this.currentHealth = maxHealth
-        this.points = 0
-        this.ticker = 0
+    constructor(team1, team2) {
+        // this.name = name
+        // this.speed = speed
+        // this.attack = attack
+        // this.defence = defence
+        // this.currentHealth = maxHealth
+        // this.points = 0
+        // this.ticker = 0
+
+        this.team1 = team1.map(spirit => new Spirit(spirit)); 
+        this.team2 = team2.map(spirit => new Spirit(spirit));
+        this.ticker = 0; 
+        this.currentCombatants = [this.team1[0], this.team2[0]]; 
+        this.winner = null; 
     }
+
+    incrementTicker() {
+        this.currentCombatants.forEach(combatant => combatant.ticker());
+      }
+
+      resolveRound() {
+        const [combatant1, combatant2] = this.currentCombatants;
+    
+        if (combatant1.ticker >= 100 && combatant2.ticker >= 100) {
+          // Higher ticker goes first
+          if (combatant1.ticker > combatant2.ticker) {
+            this.attack(combatant1, combatant2);
+          } else {
+            this.attack(combatant2, combatant1);
+          }
+        } else if (combatant1.ticker >= 100) {
+          this.attack(combatant1, combatant2);
+        } else if (combatant2.ticker >= 100) {
+          this.attack(combatant2, combatant1);
+        }
+      }
+    
+
+
+
+
     // incrementing ticker by its corresponding speed
     ticker() {
         this.ticker += this.speed
@@ -23,7 +55,9 @@ class Spirit {
         return false
     }
 
+    
+
     resetTicker() {
         this.ticker = 0
     }
-}
+}Z
